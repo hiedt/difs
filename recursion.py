afab8092d2712861e763da9a17d1c9057e975150
Dao Trung Hieu <44452839+HieuDaoTrung@users.noreply.github.com>
2020-04-19 16:14:08 +0700

    Add another traditional way to calc Factorial

--- recursion.py
+++ recursion.py
@@ -2,3 +2,10 @@ def factorial(x):
     if x == 0 or x ==1:
         return 1
     return x*factorial(x-1)
+# Another traditional way
+def facorial_old(x):
+    result = 1
+    while(x>0):
+        result = result*x
+        x -= 1
+    return result
